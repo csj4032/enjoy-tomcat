cc38c5004f67940700b6e4abfe9984a8f1fd1589
genius.choi <genius.choi@kakaocorp.com>
2017-07-03 00:06:27 +0900

    ex02.pyrmont

--- how-tomcat-works/src/main/java/ex02/pyrmont/HttpServer1.java
+++ how-tomcat-works/src/main/java/ex02/pyrmont/HttpServer1.java
@@ -1,4 +1,62 @@
 package ex02.pyrmont;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class HttpServer1 {
+
+	private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+	private boolean shutdown = false;
+
+	public static void main(String[] args) {
+		HttpServer1 server = new HttpServer1();
+		server.await();
+	}
+
+	public void await() {
+		int port = 8080;
+		try (ServerSocket serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"))) {
+			// Loop waiting for a request
+			while (!shutdown) {
+				Socket socket = null;
+				InputStream input = null;
+				OutputStream output = null;
+				try {
+					socket = serverSocket.accept();
+					input = socket.getInputStream();
+					output = socket.getOutputStream();
+
+					Request request = new Request(input);
+					request.parse();
+
+					Response response = new Response(output);
+					response.setRequest(request);
+
+					if (request.getUri().startsWith("/servlet/")) {
+						ServletProcessor1 processor = new ServletProcessor1();
+						processor.process(request, response);
+					} else {
+						StaticResourceProcessor processor = new StaticResourceProcessor();
+						processor.process(request, response);
+					}
+
+					socket.close();
+					shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ how-tomcat-works/src/main/java/ex02/pyrmont/ResponseFacade.java
@@ -0,0 +1,96 @@
+package ex02.pyrmont;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+public class ResponseFacade implements ServletResponse {
+
+	private ServletResponse response;
+
+	public ResponseFacade(Response response) {
+		this.response = response;
+	}
+
+	@Override
+	public String getCharacterEncoding() {
+		return null;
+	}
+
+	@Override
+	public String getContentType() {
+		return null;
+	}
+
+	@Override
+	public ServletOutputStream getOutputStream() throws IOException {
+		return response.getOutputStream();
+	}
+
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		return response.getWriter();
+	}
+
+	@Override
+	public void setCharacterEncoding(String charset) {
+
+	}
+
+	@Override
+	public void setContentLength(int len) {
+
+	}
+
+	@Override
+	public void setContentLengthLong(long length) {
+
+	}
+
+	@Override
+	public void setContentType(String type) {
+
+	}
+
+	@Override
+	public void setBufferSize(int size) {
+
+	}
+
+	@Override
+	public int getBufferSize() {
+		return response.getBufferSize();
+	}
+
+	@Override
+	public void flushBuffer() throws IOException {
+		response.flushBuffer();
+	}
+
+	@Override
+	public void resetBuffer() {
+
+	}
+
+	@Override
+	public boolean isCommitted() {
+		return response.isCommitted();
+	}
+
+	@Override
+	public void reset() {
+
+	}
+
+	@Override
+	public void setLocale(Locale loc) {
+
+	}
+
+	@Override
+	public Locale getLocale() {
+		return response.getLocale();
+	}
+}
